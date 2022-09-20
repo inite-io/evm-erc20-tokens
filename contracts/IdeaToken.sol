4042efdcb643cde0f5b4126068946c1b0b6467b3
Myroslav Gavryliak <myroslav.gavriliak@gmail.com>
2022-09-20 17:34:57 +0300

    fix(#2): ownable for idea token for mint

--- contracts/IdeaToken.sol
+++ contracts/IdeaToken.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract IdeaToken is ERC20, Ownable {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
--- contracts/InitToken.sol
+++ contracts/InitToken.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract InitToken is ERC20 {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
