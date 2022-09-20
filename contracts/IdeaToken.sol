15fd477e8f91a363dd86b4603017faad54d17c94
Myroslav Gavryliak <myroslav.gavriliak@gmail.com>
2022-09-20 17:27:17 +0300

    feat(#1): move mint to idea; no mint for init token

--- contracts/IdeaToken.sol
+++ contracts/IdeaToken.sol
@@ -4,8 +4,12 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract IdeaToken is ERC20 {
+contract IdeaToken is ERC20, Ownable {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
--- contracts/InitToken.sol
+++ contracts/InitToken.sol
@@ -5,12 +5,8 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract InitToken is ERC20, Ownable {
+contract InitToken is ERC20 {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
-    }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
     }
 }
